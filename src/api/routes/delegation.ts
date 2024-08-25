e41623cf2535746aed3698f7d381c024c0f6d9de
m1212e <14091540+m1212e@users.noreply.github.com>
2024-08-25 22:07:36 +0200

    ✨ feat (MUNify): add endpoint for delegation creation branch: munify-delegator/application

--- src/api/routes/delegation.ts
+++ src/api/routes/delegation.ts
@@ -1,9 +1,31 @@
+import { permissionsPlugin } from '$api/auth/permissions';
 import { CRUDMaker } from '$api/util/crudmaker';
+import { db } from '$db/db';
+import {
+	DelegationInputCreate,
+	DelegationPlain,
+} from '$db/generated/schema/Delegation';
 import Elysia from 'elysia';
+import { customAlphabet } from 'nanoid';
 
 export const delegation = new Elysia()
 	.use(CRUDMaker.getAll('delegation'))
 	.use(CRUDMaker.getOne('delegation'))
-	.use(CRUDMaker.createOne('delegation'))
 	.use(CRUDMaker.updateOne('delegation'))
-	.use(CRUDMaker.deleteOne('delegation'));
+	.use(CRUDMaker.deleteOne('delegation'))
+	.use(permissionsPlugin)
+	.post(
+		`/delegation`,
+		async ({ permissions, body }) => {
+			permissions.checkIf((user) => user.can('create', 'Delegation'));
+			// https://github.com/CyberAP/nanoid-dictionary
+			const entrycode = customAlphabet('6789BCDFGHJKLMNPQRTW', 6);
+			return await db.delegation.create({
+				data: { ...body, entryCode: entrycode() }
+			});
+		},
+		{
+			body: DelegationInputCreate,
+			response: DelegationPlain
+		}
+	);
