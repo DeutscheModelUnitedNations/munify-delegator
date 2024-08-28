4b735d301ad6e5b13f48918e7e0061ec41565fb3
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2024-08-28 16:41:25 +0200

    ✨ feat (App Frontend): Absolute Profile Button branch: frontend

---
+++ src/lib/components/ProfileButton/ProfileButton.svelte
@@ -0,0 +1,14 @@
+<ul tabindex="-1" class="menu dropdown-content mt-3 w-52 rounded-box bg-base-100 p-2 shadow-2xl">
+	<li>
+		<a href="/my-account">
+			<i class="fa-duotone fa-user w-4"></i>
+			Profilverwaltung
+		</a>
+	</li>
+	<li>
+		<a>
+			<i class="fa-duotone fa-sign-out w-4"></i>
+			Logout
+		</a>
+	</li>
+</ul>
