50ff69c48793b4eae1963047fc2795fa41c3ce73
m1212e <14091540+m1212e@users.noreply.github.com>
2024-09-23 19:27:31 +0200

    🚧 wip (MUNify): branch: Branch not found

--- src/routes/(authenticated)/management/+page.ts
+++ src/routes/(authenticated)/management/+page.ts
@@ -1,20 +1,7 @@
-import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import { apiClient } from '$api/client';
+import { loadApiHandler } from '$lib/helper/loadApiHandler';
 
-export const load: PageLoad = async ({ params, fetch, url }) => {
-	//TODO
-	const conferences = await apiClient({ fetch, origin: url.origin }).conference.get();
-
-	return {
-		conferences: [
-			{
-				id: '1',
-				location: 'Kiel',
-				start: new Date('2025-03-01'),
-				end: new Date('2025-03-05'),
-				title: 'MUN-SH 2025'
-			}
-		]
-	};
-};
+export const load: PageLoad = loadApiHandler(async ({ api }) => {
+	const conferences = await api.conference.get();
+	return { conferences };
+});
