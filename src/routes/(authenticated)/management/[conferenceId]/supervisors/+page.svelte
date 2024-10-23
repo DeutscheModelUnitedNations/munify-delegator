3ca92b3a2b0de91a75e4675f751cc11c8e6c2376
m1212e <14091540+m1212e@users.noreply.github.com>
2024-10-23 20:47:23 +0200

    ✨ feat: add supervisor table

---
+++ src/routes/(authenticated)/management/[conferenceId]/supervisors/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	// import ManagementHeader from '$lib/components/ManagementHeader.svelte';
+	// import PrintHeader from '$lib/components/DataTable/PrintHeader.svelte';
+	import { type TableColumns } from 'svelte-table';
+	import * as m from '$lib/paraglide/messages';
+	import type { PageData } from './$houdini';
+	import { getTableSettings } from '$lib/components/DataTable/dataTableSettings.svelte';
+	import DataTable from '$lib/components/DataTable/DataTable.svelte';
+	import IndividualDrawer from './SupervisorDrawer.svelte';
+
+	const { data }: { data: PageData } = $props();
+	const queryData = $derived(data.ConferenceSupervisorsQuery);
+	const supervisors = $derived($queryData?.data?.findManyConferenceSupervisors ?? []);
+	const { getTableSize } = getTableSettings();
+
+	const columns: TableColumns<(typeof supervisors)[number]> = [
+		{
+			key: 'name',
+			title: m.name(),
+			value: (row) => `${row.user.family_name} ${row.user.given_name} `,
+			renderValue: (row) =>
+				`<span class="uppercase">${row.user.family_name}</span> ${row.user.given_name} `,
+			sortable: true,
+			parseHTML: true
+		},
+		{
+			key: 'plansAttendance',
+			title: m.adminPlansAttendance(),
+			value: (row) => (row.plansOwnAttendenceAtConference ? 1 : 0),
+			renderValue: (row) =>
+				row.plansOwnAttendenceAtConference
+					? `<i class="fa-duotone fa-location-check text-${getTableSize()}"></i>`
+					: `<i class="fa-duotone fa-cloud text-${getTableSize()}"></i>`,
+			parseHTML: true,
+			sortable: true,
+			class: 'text-center',
+			headerClass: 'text-center'
+		},
+		{
+			key: 'delegations',
+			title: m.adminDelegations(),
+			value: (row) => row.delegations.length,
+			sortable: true,
+			class: 'text-center',
+			headerClass: 'text-center'
+		}
+	];
+
+	let selectedSupervisorRow = $state<(typeof supervisors)[number]>();
+	// TODO export data
+</script>
+
+<DataTable
+	{columns}
+	rows={supervisors}
+	enableSearch={true}
+	queryParamKey="filter"
+	rowSelected={(row) => {
+		selectedSupervisorRow = row;
+	}}
+/>
+
+{#if selectedSupervisorRow}
+	<IndividualDrawer
+		supervisorId={selectedSupervisorRow.id}
+		conferenceId={data.conferenceId}
+		open={selectedSupervisorRow !== undefined}
+		onClose={() => (selectedSupervisorRow = undefined)}
+	/>
+{/if}
