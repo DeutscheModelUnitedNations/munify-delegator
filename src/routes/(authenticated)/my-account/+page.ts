de2f608761fb64ce0f2f783ef36975f8c8a53998
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2024-11-30 04:23:44 +0500

    Sanitize redirect URL for security in my-account page
    Improved the `redirectUrl` handling by validating its host against the current URL's host to prevent potential misuse of the parameter. Ensures safer redirection in the `my-account` route.

--- src/routes/(authenticated)/my-account/+page.ts
+++ src/routes/(authenticated)/my-account/+page.ts
@@ -4,11 +4,15 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = loadApiHandler(async ({ url, api, parent }) => {
 	const { user } = await parent();
+	let redirectUrl = url.searchParams.get('redirect');
+	if (redirectUrl && new URL(redirectUrl).host !== url.host) {
+		redirectUrl = null;
+	}
 	await api.auth['trigger-user-data-refresh'].post();
 	const fullUser = await checkForError(api.user({ id: user.sub }).get());
 	const { mySystemRoles } = await checkForError(api.auth['my-system-roles'].get());
 	return {
-		redirectUrl: url.searchParams.get('redirect') || null,
+		redirectUrl,
 		fullUser,
 		mySystemRoles,
 		url
