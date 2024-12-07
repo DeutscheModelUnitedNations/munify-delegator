3720b4a5020e98828e3f24f71bd210ecc4cad62d
m1212e <14091540+m1212e@users.noreply.github.com>
2024-12-07 19:46:57 +0100

    🐞 fix: type errors

--- src/routes/(authenticated)/dashboard/[conferenceId]/stages/SingleParticipantPreparationStage.svelte
+++ src/routes/(authenticated)/dashboard/[conferenceId]/stages/SingleParticipantPreparationStage.svelte
@@ -1,1 +1,11 @@
-Test
+<script lang="ts">
+	import type { StoresValues } from '$lib/services/storeExtractorType';
+	import type { PageData } from '../$houdini';
+
+	let {
+		data
+	}: {
+		data: NonNullable<StoresValues<PageData['MyConferenceparticipationQuery']>['data']> &
+			Pick<PageData, 'user'>;
+	} = $props();
+</script>
