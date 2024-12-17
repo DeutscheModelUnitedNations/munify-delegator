2a41363118fd486b4f05e08956fdf9468c8fe991
m1212e <14091540+m1212e@users.noreply.github.com>
2024-12-17 15:43:36 +0100

    ✨ feature: adds otel support for logs (#157)
    * 🚧 wip: add basic otel traces
    
    * 🧼 format & lint
    
    * ✨ feat: add otel support
    
    * 🧼 format & lint
    
    * ⬆️ improvement: disable dev otel by default
    
    * 🐞 fix: add missing deps

--- src/api/resolvers/api.ts
+++ src/api/resolvers/api.ts
@@ -1,7 +1,9 @@
+import { graphqlYogaTracerPlugin } from './tracer';
 import { createYoga } from 'graphql-yoga';
 import { builder } from './builder';
 import type { RequestEvent } from '@sveltejs/kit';
 import { context } from '$api/context/context';
+import { maskError } from './errors';
 
 builder.queryType({});
 builder.mutationType({});
@@ -23,9 +25,9 @@ import './modules/singleParticipant';
 import './modules/teamMember';
 import './modules/user';
 import './modules/assignments';
-import { maskError } from './errors';
 
 export const yogaInstance = createYoga<RequestEvent>({
+	plugins: [graphqlYogaTracerPlugin],
 	schema: builder.toSchema(),
 	graphqlEndpoint: '/api/graphql',
 	fetchAPI: { Response },
---
+++ src/api/resolvers/tracer.ts
@@ -0,0 +1,91 @@
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import prisInstru from '@prisma/instrumentation';
+import { Resource } from '@opentelemetry/resources';
+import { configPrivate } from '$config/private';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { trace } from '@opentelemetry/api';
+import type { Plugin } from 'graphql-yoga';
+import {
+	SimpleSpanProcessor,
+	BatchSpanProcessor,
+	type SpanProcessor
+} from '@opentelemetry/sdk-trace-base';
+import { configPublic } from '$config/public';
+import { AttributeNames, SpanNames } from '@pothos/tracing-opentelemetry';
+import { print } from 'graphql';
+
+const headers: Record<string, string> = {};
+
+if (configPrivate.OTEL_AUTHORIZATION_HEADER) {
+	headers.authorization = configPrivate.OTEL_AUTHORIZATION_HEADER;
+}
+
+const exporter = configPrivate.OTEL_ENDPOINT_URL
+	? new OTLPTraceExporter({
+			url: configPrivate.OTEL_ENDPOINT_URL,
+			headers
+		})
+	: undefined;
+
+const processors: SpanProcessor[] = [];
+if (exporter) {
+	if (configPrivate.NODE_ENV !== 'production') {
+		processors.push(new SimpleSpanProcessor(exporter));
+	} else {
+		processors.push(new BatchSpanProcessor(exporter));
+	}
+}
+
+const provider = new NodeTracerProvider({
+	spanProcessors: processors,
+	resource: new Resource({
+		[ATTR_SERVICE_NAME]: configPrivate.OTEL_SERVICE_NAME,
+		[ATTR_SERVICE_VERSION]:
+			configPrivate.OTEL_SERVICE_VERSION ?? configPublic.PUBLIC_VERSION ?? 'unknown'
+	})
+});
+
+registerInstrumentations({
+	tracerProvider: provider,
+	instrumentations: [
+		new HttpInstrumentation(),
+		new prisInstru.PrismaInstrumentation({
+			middleware: true
+		})
+	]
+});
+
+provider.register();
+
+export const tracer = trace.getTracer('graphql');
+
+export const graphqlYogaTracerPlugin: Plugin = {
+	onExecute: ({ setExecuteFn, executeFn }) => {
+		setExecuteFn((options) =>
+			tracer.startActiveSpan(
+				SpanNames.EXECUTE,
+				{
+					attributes: {
+						[AttributeNames.OPERATION_NAME]: options.operationName ?? undefined,
+						[AttributeNames.SOURCE]: print(options.document)
+					}
+				},
+				async (span) => {
+					try {
+						const result = await executeFn(options);
+
+						return result;
+					} catch (error) {
+						span.recordException(error as Error);
+						throw error;
+					} finally {
+						span.end();
+					}
+				}
+			)
+		);
+	}
+};
