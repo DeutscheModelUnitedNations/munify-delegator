a6c2f5763f639c8b79fe8a1c8ebec0eae06ce921
Tade Strehk <git@strehk.eu>
2025-01-09 00:03:55 +0100

    ⬆️ improvement: Small last minute improvements

--- src/routes/(authenticated)/management/[conferenceId]/configuration/+page.svelte
+++ src/routes/(authenticated)/management/[conferenceId]/configuration/+page.svelte
@@ -53,6 +53,7 @@
 
 <div class="card-body rounded-2xl bg-base-100 dark:bg-base-200">
 	<Form {form}>
+		<h3 class="mt-8 text-lg font-bold">{m.general()}</h3>
 		<FormTextInput
 			{form}
 			name="title"
@@ -84,7 +85,14 @@
 		<FormDateTimeInput {form} name="startAssignment" label={m.conferenceStartAssignment()} />
 		<FormDateTimeInput {form} name="startConference" label={m.conferenceStart()} />
 		<FormDateTimeInput {form} name="endConference" label={m.conferenceEnd()} />
+
+		<h3 class="mt-8 text-lg font-bold">{m.conferenceStatus()}</h3>
 		<FormSelect {form} name="state" label={m.conferenceStatus()} options={conferenceStateOptions} />
+		<div class="prose">
+			{@html m.conferenceStatusDescription()}
+		</div>
+
+		<h3 class="mt-8 text-lg font-bold">{m.communication()}</h3>
 		<div class="flex w-full items-center gap-2">
 			<FormTextArea {form} name="info" placeholder={'Info...'} label={m.infos()} />
 			<button
@@ -105,7 +113,17 @@
 		/>
 
 		<h3 class="mt-8 text-lg font-bold">{m.bankingInformation()}</h3>
-		<FormCheckbox {form} name="unlockPayments" label={m.paymentOpen()} />
+		<FormCheckbox
+			{form}
+			name="unlockPayments"
+			label={m.paymentOpen()}
+			disabled={!$formData.feeAmount ||
+				!$formData.bankName ||
+				!$formData.iban ||
+				!$formData.bic ||
+				!$formData.accountHolder ||
+				!$formData.currency}
+		/>
 		<FormTextInput {form} name="feeAmount" placeholder={'75,00'} label={m.fee()} type="number" />
 		<FormTextInput {form} name="bankName" placeholder={'Bank Name'} label={m.bankName()} />
 		<FormTextInput {form} name="iban" placeholder={'DE12345678901234567890'} label={m.iban()} />
@@ -119,7 +137,16 @@
 		<FormTextInput {form} name="currency" placeholder={'EUR'} label={m.currency()} />
 
 		<h3 class="mt-8 text-lg font-bold">{m.postalRegistration()}</h3>
-		<FormCheckbox {form} name="unlockPostals" label={m.postalOpen()} />
+		<FormCheckbox
+			{form}
+			name="unlockPostals"
+			label={m.postalOpen()}
+			disabled={!$formData.postalName ||
+				!$formData.postalStreet ||
+				!$formData.postalZip ||
+				!$formData.postalCity ||
+				!$formData.postalCountry}
+		/>
 		<FormTextInput {form} name="postalName" placeholder={m.name()} label={m.name()} />
 		<FormTextInput {form} name="postalStreet" placeholder={m.street()} label={m.street()} />
 		<FormTextInput
