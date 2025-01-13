e227f68e03f4e0cffad7e5e639ad018b749c5658
Tade Strehk <git@strehk.eu>
2025-01-13 01:47:39 +0100

    🐞 fix: scrolling issue

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -27,9 +27,12 @@
 	<div
 		aria-hidden="true"
 		onclick={close}
-		class="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-30"
+		class="fixed left-0 top-0 z-30 h-screen w-screen bg-black opacity-30"
 	></div>
-	<div class="fixed right-0 top-0 z-20 h-screen duration-300" style="width: {width}">
+	<div
+		class="no-scrollbar fixed right-0 top-0 z-40 h-screen overflow-y-auto duration-300"
+		style="width: {width}"
+	>
 		<div class="flex min-h-full flex-col gap-8 bg-base-100 p-10">
 			{@render children()}
 			<button class="btn absolute right-4 top-4" onclick={close} aria-label="Close">
@@ -38,3 +41,16 @@
 		</div>
 	</div>
 {/if}
+
+<style lang="postcss">
+	/* Hide scrollbar for Chrome, Safari and Opera */
+	.no-scrollbar::-webkit-scrollbar {
+		display: none;
+	}
+
+	/* Hide scrollbar for IE, Edge and Firefox */
+	.no-scrollbar {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+</style>
