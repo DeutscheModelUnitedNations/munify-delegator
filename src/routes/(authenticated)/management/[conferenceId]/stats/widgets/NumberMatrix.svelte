25711bc22b3b7cd1aeec5b1bc79807d5d2efac02
Tade Strehk <git@strehk.eu>
2025-01-29 16:57:53 +0100

    🐞 fix: small fixes

--- src/routes/(authenticated)/management/[conferenceId]/stats/widgets/NumberMatrix.svelte
+++ src/routes/(authenticated)/management/[conferenceId]/stats/widgets/NumberMatrix.svelte
@@ -10,46 +10,48 @@
 </script>
 
 <div
-	class="col-span-2 overflow-x-auto rounded-3xl bg-base-200 p-6 shadow-sm sm:col-span-1 md:col-span-6"
+	class="col-span-2 overflow-x-auto rounded-2xl bg-base-200 p-6 shadow-sm sm:col-span-2 md:col-span-12 xl:col-span-6"
 >
 	<h1 class="stat-title w-full">{title}</h1>
-	<table class="table w-full">
-		<!-- head -->
-		<thead>
-			<tr>
-				<th></th>
-				{#each xLabels as label}
-					<th>{label}</th>
-				{/each}
-			</tr>
-		</thead>
-		<tbody>
-			{#each data as rowData, i}
+	<div class="overflow-x-auto">
+		<table class="table w-full">
+			<!-- head -->
+			<thead>
 				<tr>
-					<th>
-						{#if typeof yLabels[i] === 'string'}
-							{yLabels[i]}
-						{:else}
-							<i class="fa-duotone {yLabels[i].icon}"></i>
-							{yLabels[i].label}
-						{/if}
-					</th>
-					{#each rowData as entry}
-						<td>{entry}</td>
+					<th></th>
+					{#each xLabels as label}
+						<th>{label}</th>
 					{/each}
-					<!-- row sums -->
-					<td><b>{rowData.reduce((a, b) => a + b, 0)}</b></td>
 				</tr>
-			{/each}
-			<tr>
-				<th></th>
-				<!-- col sums -->
-				{#each data[0] as _, i}
-					<td><b>{data.reduce((a, b) => a + b[i], 0)}</b></td>
+			</thead>
+			<tbody>
+				{#each data as rowData, i}
+					<tr>
+						<th>
+							{#if typeof yLabels[i] === 'string'}
+								{yLabels[i]}
+							{:else}
+								<i class="fa-duotone {yLabels[i].icon}"></i>
+								{yLabels[i].label}
+							{/if}
+						</th>
+						{#each rowData as entry}
+							<td>{entry}</td>
+						{/each}
+						<!-- row sums -->
+						<td><b>{rowData.reduce((a, b) => a + b, 0)}</b></td>
+					</tr>
 				{/each}
-				<!-- total -->
-				<td><b>{data.reduce((a, b) => a + b.reduce((a, b) => a + b, 0), 0)}</b></td>
-			</tr>
-		</tbody>
-	</table>
+				<tr>
+					<th></th>
+					<!-- col sums -->
+					{#each data[0] as _, i}
+						<td><b>{data.reduce((a, b) => a + b[i], 0)}</b></td>
+					{/each}
+					<!-- total -->
+					<td><b>{data.reduce((a, b) => a + b.reduce((a, b) => a + b, 0), 0)}</b></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
