9cd5e04429f5b3cbddd0803c7a9ea1bd25f90ccf
Tade Strehk <git@strehk.eu>
2025-04-01 01:24:12 +0200

    ✨ chore: remove unused PDF generation code from test.ts

--- test.ts
+++
@@ -1,7 +0,0 @@
-import { PDFDocument, rgb, StandardFonts, PageSizes, PDFPage, PDFFont } from 'pdf-lib';
-
-const pdfDoc = await PDFDocument.create();
-const page = pdfDoc.addPage(PageSizes.A4);
-const { width, height } = page.getSize();
-
-console.log(page, width, height);
