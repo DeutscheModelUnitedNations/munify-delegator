cef599d4a12ac1bef05bdfd2d16b059e724bee5f
m1212e <14091540+m1212e@users.noreply.github.com>
2025-09-10 23:13:05 +0200

    feat: add max length validation to delegation/individual form schema

--- src/routes/(authenticated)/registration/[conferenceId]/create-delegation/form-schema.ts
+++ src/routes/(authenticated)/registration/[conferenceId]/create-delegation/form-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const createDelegationFormSchema = z.object({
-	school: z.string().nullish(),
-	motivation: z.string().nullish(),
-	experience: z.string().nullish()
+	school: z.string().max(100).nullish(),
+	motivation: z.string().max(500).nullish(),
+	experience: z.string().max(500).nullish()
 });
