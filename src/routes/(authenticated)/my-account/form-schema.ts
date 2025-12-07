8f348d5558fdda178dac0edcfe088bdd60ad7888
Tade Strehk <git@strehk.eu>
2025-12-07 14:53:01 +0100

    use google phone library for validation

--- src/routes/(authenticated)/my-account/form-schema.ts
+++ src/routes/(authenticated)/my-account/form-schema.ts
@@ -2,7 +2,11 @@ import valiator from 'validator';
 import { z } from 'zod';
 import { m } from '$lib/paraglide/messages';
 import { getLocale } from '$lib/paraglide/runtime';
-import { isValidPhoneNumber } from '$lib/utils/phoneValidation';
+import {
+	isValidPhoneNumber,
+	parsePhoneNumberFromString,
+	findPhoneNumbersInText
+} from 'libphonenumber-js';
 
 // must be at least 13 years old
 const birthdayMaxDate = new Date(Date.now() - 13 * 365 * 24 * 60 * 60 * 1000);
@@ -15,10 +19,15 @@ export const userFormSchema = z.object({
 	}),
 	phone: z
 		.string()
-		.transform((s) => s.replaceAll(' ', ''))
-		.transform((s) => s.replaceAll('-', ''))
-		.refine((s) => isValidPhoneNumber(s, true), {
+		.refine((s) => isValidPhoneNumber(s), {
 			message: m.pleaseEnterAValidPhoneNumber()
+		})
+		.transform((s) => {
+			const phoneNumber = parsePhoneNumberFromString(s);
+			if (phoneNumber) {
+				return phoneNumber.formatInternational();
+			}
+			return s;
 		}),
 	street: z.string().min(3, {
 		message: m.atLeastXChars({ amount: 3 })
@@ -39,7 +48,19 @@ export const userFormSchema = z.object({
 	]),
 	pronouns: z.string().nullish(),
 	foodPreference: z.string().refine((s) => ['OMNIVORE', 'VEGETARIAN', 'VEGAN'].includes(s)),
-	emergencyContacts: z.string().min(5),
+	emergencyContacts: z
+		.string()
+		.min(5)
+		.transform((s) => {
+			const phoneNumbers = findPhoneNumbersInText(s);
+			let res: string = '';
+			for (const output of phoneNumbers) {
+				const partBefore = s.substring(0, output.startsAt);
+				const partAfter = s.substring(output.endsAt);
+				res = partBefore + output.number.formatInternational() + partAfter;
+			}
+			return res || s;
+		}),
 	wantsToReceiveGeneralInformation: z.boolean().default(false),
 	wantsJoinTeamInformation: z.boolean().default(false)
 });
