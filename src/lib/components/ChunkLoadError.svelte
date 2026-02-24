4248676424d5111ff9b73fc3814974dda1c8b57c
Tade Strehk <git@strehk.eu>
2026-02-24 14:03:15 +0100

    Streamline Error Message UI

--- src/lib/components/ChunkLoadError.svelte
+++ src/lib/components/ChunkLoadError.svelte
@@ -11,14 +11,15 @@
 
 <div class="alert alert-error alert-soft">
 	<i class="fa-solid fa-triangle-exclamation text-xl"></i>
-	<div class="flex-1">
+	<div class="flex-1 w-full">
 		<p>{m.chunkLoadErrorMessage()}</p>
 		<button class="btn btn-ghost btn-xs mt-1" onclick={() => (expanded = !expanded)}>
 			{m.chunkLoadErrorDetails()}
 			<i class="fa-solid fa-chevron-{expanded ? 'up' : 'down'} ml-1"></i>
 		</button>
 		{#if expanded}
-			<pre class="bg-base-300 mt-2 overflow-x-auto rounded p-2 text-xs">{error}</pre>
+			<pre
+				class="bg-base-300 text-base-content mt-2 overflow-x-auto rounded p-2 text-xs w-full">{error}</pre>
 		{/if}
 	</div>
 	<button class="btn btn-error btn-sm" onclick={() => location.reload()}>
