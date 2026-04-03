8b2d9c2c99bdbfcdcdbcd3dec1891fb98ca63fed
ilzhoefe <maximilian.ilzhoefer@uni-hohenheim.de>
2026-04-03 23:37:57 +0200

    feat: add user breadcrumb and redirect to participants page

---
+++ src/routes/(authenticated)/management/[conferenceId]/user/+page.server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	throw redirect(303, `/management/${params.conferenceId}/participants`);
+	throw redirect(303, ``);
+};
